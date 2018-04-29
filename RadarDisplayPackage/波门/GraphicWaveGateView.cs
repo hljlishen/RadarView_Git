17fb93717ba16d15c98afa1d0e34ec9397f4046c
Lishen <hljlishen@hotmail.com>
2018-04-30 07:36:28 +0800

    修改waveGateView构造函数，不再对ProjectedDis赋值，改为对Dis赋值

--- RadarDisplayPackage/波门/GraphicWaveGateView.cs
+++ RadarDisplayPackage/波门/GraphicWaveGateView.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.WindowsAPICodePack.DirectX.Direct2D1;
+using System;
+using System.Drawing;
 using TargetManagerPackage;
-using Microsoft.WindowsAPICodePack.DirectX.Direct2D1;
-using System.Windows.Forms;
-using System.Drawing; 
 
 
 namespace RadarDisplayPackage
@@ -31,23 +26,26 @@ namespace RadarDisplayPackage
             
             this.waveGate = waveGate;
 
-            if(waveGate.IsSemiAuto)
-                waveGateBrush = canvas.CreateSolidColorBrush(GraphicTrackDisplayer.GetColorFFromRgb(255 ,255 ,0));   //黄色
-            else
-                waveGateBrush = canvas.CreateSolidColorBrush(GraphicTrackDisplayer.GetColorFFromRgb(245, 222, 179));   //淡黄色
+            waveGateBrush = canvas.CreateSolidColorBrush(waveGate.IsSemiAuto ? 
+                GraphicTrackDisplayer.GetColorFFromRgb(255 ,255 ,0) : 
+                GraphicTrackDisplayer.GetColorFFromRgb(245, 222, 179));
 
             coodinateSystem = displayer.coordinateSystem;
 
             //Selected = false;
 
-            PolarCoordinate c = new PolarCoordinate();
+            PolarCoordinate c = new PolarCoordinate
+            {
+                Az = waveGate.BeginAngle,
+                //ProjectedDis = waveGate.BeginDistance;    //之前版本将距离赋给ProjectedDis
+                Dis = waveGate.BeginDistance
+            };
 
-            c.Az = waveGate.BeginAngle;
-            c.ProjectedDis = waveGate.BeginDistance;
             innerLeft = coodinateSystem.CoordinateToPoint(c);
 
             c.Az = waveGate.EndAngle;
-            c.ProjectedDis = waveGate.EndDistance;
+            //c.ProjectedDis = waveGate.EndDistance;  //之前版本将距离赋给projectedDis
+            c.Dis = waveGate.EndDistance;
             outterRight = coodinateSystem.CoordinateToPoint(c);
 
             geometry = coodinateSystem.BuildWaveGateGeometry(innerLeft, outterRight);
@@ -58,22 +56,7 @@ namespace RadarDisplayPackage
             StrokeStyleProperties ssp = new StrokeStyleProperties();
             if (Selected)   //如果选中，填充区域coo
             {
-                waveGateBrush.Opacity = 0.6f;
-                if (dashStyleCount < dashStyleTimes)
-                {
-                    dashStyleCount++;
-                    ssp.DashStyle = DashStyle.DashDot;
-                }
-                else if (dashStyleCount < dashStyleTimes * 2)
-                {
-                    dashStyleCount++;
-                    ssp.DashStyle = DashStyle.Dash;
-                }
-                else
-                {
-                    dashStyleCount = 0;
-                    ssp.DashStyle = DashStyle.Dash;
-                }
+                ssp = GetSelectedWaveGateStrokeStyleProperties();
             }
             else
             {
@@ -90,34 +73,43 @@ namespace RadarDisplayPackage
             canvas.DrawGeometry(geometry, waveGateBrush, 1.5f, ss);
         }
 
-        public WaveGate WaveGate
+        private StrokeStyleProperties GetSelectedWaveGateStrokeStyleProperties()
         {
-            get
+            StrokeStyleProperties ssp = new StrokeStyleProperties();
+            waveGateBrush.Opacity = 0.6f;
+            if (dashStyleCount < dashStyleTimes)
             {
-                return waveGate;
+                dashStyleCount++;
+                ssp.DashStyle = DashStyle.DashDot;
             }
+            else if (dashStyleCount < dashStyleTimes * 2)
+            {
+                dashStyleCount++;
+                ssp.DashStyle = DashStyle.Dash;
+            }
+            else
+            {
+                dashStyleCount = 0;
+                ssp.DashStyle = DashStyle.Dash;
+            }
+
+            return ssp;
         }
+
+        public WaveGate WaveGate => waveGate;
 
         public bool Selected
         {
-            get
-            {
-                return waveGate.Active;
-            }
+            get => waveGate.Active;
 
-            set
-            {
-                waveGate.Active = value;
-            }
+            set => waveGate.Active = value;
         }
 
         public void HandleMouseClick(Point p)
         {
             Point2F p1 = CoordinateSystem.PointToPoint2F(p);
             if(IsPointInView(p1))    //如果点在波门范围内
-            {
                 Selected = !Selected;   //selected取反
-            }
         }
 
         private bool IsPointInView(Point2F point)
